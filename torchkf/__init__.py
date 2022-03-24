a99058d5528d5425e291c1bacd0a058c187bbe50
Johan Medrano <jmedrano@lirmm.fr>
2022-03-24 12:44:30 +0100

    Add HDM forward - passes first tests

--- torchkf/__init__.py
+++ torchkf/__init__.py
@@ -2,3 +2,4 @@ from .state_space_model import GaussianStateSpaceModel
 from .transformations import Gaussian, GaussianTransform, LinearTransform, LinearizedTransform, UnscentedTransform, \
     stack_distributions
 from .utils import plot_traj
+from .hierarchical_dynamical_model import GaussianSystem, HierarchicalDynamicalModel
