4f7fac4e99c3c6c605db8ff036df12969a8e64a1
Johan Medrano <jmedrano@lirmm.fr>
2022-05-02 17:32:06 +0200

    [dem] first complete run

--- torchkf/dem.py
+++ torchkf/dem.py
@@ -107,9 +107,11 @@ def compute_df_d2f(func, inputs, input_keys=None) -> Tuple[dotdict, dotdict]:
     # dim 3 of H are df[k]/d(x[:]x[:])
     return df, d2f
 
-def compute_dx(f, dfdu, t): 
+def compute_dx(f, dfdu, t, isreg=False): 
     if len(f.shape) == 1: 
         f = f.unsqueeze(-1)
+    if isreg:
+        t  = np.exp(t - torch.logdet(dfdu)/f.shape[0]);
     J = torch.Tensor(block_matrix([[np.zeros((1,1)), []], [f * t, t * dfdu]]))
     dx = torch.linalg.matrix_exp(J)
     return dx[1:, 0, None]
@@ -618,9 +620,12 @@ class DEMInversion:
         qp : dotdict             = dotdict()  # loop variable for qP
         qh : dotdict             = dotdict()  # loop variable for qH
 
-        # Errors
+        # loop variables 
         # ------
-        qE : List[dotdict]       = list() 
+        qE : List[dotdict]       = list()           # errors
+        B  : dotdict             = dotdict()        # saved states
+        F  : torch.Tensor        = torch.zeros(nE)  # Free-energy
+        A  : torch.Tensor        = torch.zeros(nE)  # Free-action
 
         # prior moments
         # -------------
@@ -719,7 +724,7 @@ class DEMInversion:
             M[i].p  = Ui.shape[1]               # number of qp.p
 
             qp.u.append(Ui)                     # basis for parameters (U from SVD's USV')
-            qp.p.append(torch.zeros(M[i].p))    # initial qp.p 
+            qp.p.append(torch.zeros((M[i].p, 1)))    # initial qp.p 
             pp.c.append(Ui.T @ M[i].pC @ Ui)    # prior covariance
 
         Up = torch.block_diag(*qp.u)
@@ -730,6 +735,8 @@ class DEMInversion:
         nb    = x.shape[-1]                     # number of confounds
         nn    = nb * ny                         # number of nuisance parameters
         nf    = nP + nn                         # number of free parameters
+        ip    = slice(1,nP)
+        ib    = slice(nP,nP + nn)
         pp.c  = torch.block_diag(*pp.c)
         pp.ic = torch.linalg.inv(pp.c)
         pp.p  = torch.cat(qp.p)
@@ -845,6 +852,7 @@ class DEMInversion:
                     # NotImplemented 
 
                     print("Loop: ", iD, iT)
+                    print({k:v.shape for k,v in qu.items()})
                     # evaluatefunction: 
                     # E = v - g(x,v) and derivatives dE.dx
                     # ====================================
@@ -909,8 +917,8 @@ class DEMInversion:
                     du = compute_dx(f, dfdu, td)
                     q  = u + du
 
-                    qu.x = q[:n]
-                    qu.v = q[n:n+d]
+                    qu.x = q[:n * nx].reshape((n, nx))
+                    qu.v = q[n * nx:n * nx + d * nv].reshape((d, nv))
 
                     # ... 
 
@@ -957,8 +965,9 @@ class DEMInversion:
                 dFdhh = dFdhh - ph.ic
 
                 # update ReML extimate of parameters
-                dh = ValueError()
-                dh = max(min(dh, 2), -2)
+                dh = compute_dx(dFdh, dFdhh, tm, isreg=True)
+
+                dh = torch.clamp(dh, -2, 2)
                 qh.h = qh.h + dh 
                 mh = mh + dh
 
@@ -970,7 +979,7 @@ class DEMInversion:
 
             # conditional precision of parameters
             # -----------------------------------
-            qp.ic = qp.ic + pp.ic
+            qp.ic[ip, ip] = qp.ic[ip, ip] + pp.ic
             qp.c = torch.linalg.inv(qp.ic)
 
             # evaluate objective function F
@@ -978,19 +987,24 @@ class DEMInversion:
 
             # free-energy and action 
             # ----------------------
-            Lu = - torch.trace(iS[je, je] * EE[je, je]) / 2 \
+            Lu = - torch.trace(iS @ EE) / 2 \
                  - n * ny * np.log(2 * np.pi) * nT / 2\
-                 + torch.logdet(iS[je, je]) * nT / 2\
+                 + torch.logdet(iS) * nT / 2\
                  + iqu.c / (2*nD)
+
+            # Lu = - torch.trace(iS[je, je] * EE[je, je]) / 2 \
+            #      - n * ny * np.log(2 * np.pi) * nT / 2\
+            #      + torch.logdet(iS[je, je]) * nT / 2\
+            #      + iqu.c / (2*nD)
 
             Lp = - torch.trace(qp.e.T @ pp.ic @ qp.e) / 2\
                  - torch.trace(qh.e.T @ ph.ic @ qh.e) / 2\
-                 + torch.logdet(qp.c @ pp.ic) / 2\
+                 + torch.logdet(qp.c[ip, ip] @ pp.ic) / 2\
                  + torch.logdet(qh.c @ ph.ic) / 2
 
             La = - torch.trace(qp.e.T @ pp.ic @ qp.e) * nT / 2\
                  - torch.trace(qh.e.T @ ph.ic @ qh.e) * nT / 2\
-                 + torch.logdet(qp.c @ pp.ic * nT) * nT / 2\
+                 + torch.logdet(qp.c[ip, ip] @ pp.ic * nT) * nT / 2\
                  + torch.logdet(qh.c @ ph.ic * nT) * nT / 2
 
             Li = Lu + Lp
@@ -1012,15 +1026,15 @@ class DEMInversion:
                 
                 # gradients and curvatures
                 # ------------------------
-                dFdp[ip]     = dFdp(ip)     - pp.ic*(qp.e - pp.p);
-                dFdpp[ip,ip] = dFdpp(ip,ip) - pp.ic;
+                dFdp[ip]      = dFdp[ip]  - pp.ic*(qp.e - pp.p);
+                dFdpp[ip, ip] = dFdpp[ip, ip] - pp.ic;
                 
                 # update conditional expectation
                 # ------------------------------
-                dp      = spm_dx(dFdpp,dFdp,{te}); ... 
-                qp.e    = qp.e + dp(ip);
-                qp.p    = spm_unvec(qp.e,qp.p);
-                qp.b    = spm_unvec(dp(ib),qp.b);
+                dp      = compute_dx(dFdpp,dFdp,te, isreg=True) 
+                qp.e    = qp.e + dp[ip]
+                qp.p    = qp.e
+                qp.b    = dp[ib]
 
             else:
                 
@@ -1035,137 +1049,5 @@ class DEMInversion:
                 
             
             F[iE]  = Fi;
-            A[iE]   = Ai;
+            A[iE]  = Ai;
 
-
-
-        # # E step 
-        # for iE in range(nE): 
-        #     # D step
-        #     for iT in range(nT): 
-        #         update_x()
-        #     # M step 
-        #     for iM in range(nM): 
-        #         update_h()
-            
-        #     fa[iT] = compute_fa()
-        #     if fa[iT] > fa[iT - 1]: 
-        #         update_p()
-
- 
-
-
-# class DEMHierarchicalDynamicalModel:
-#     def __init__(self, systems: List[GaussianModel], order=4):
-#         DEMHierarchicalDynamicalModel.check_systems(systems)
-#         self._systems = systems
-#         self._n_systems = len(systems)
-#         self._input_dim = systems[0]['input_dim']
-#         self._obs_dim = systems[-1]['obs_dim']
-#         self._input_mean = input_mean
-#         self._input_cov = input_cov
-#         self._order = order
-
-#     @staticmethod
-#     def check_systems(systems):
-#         if len(systems) < 2:
-#             return
-
-#         for k in range(1, len(systems)):
-#             if not systems[k-1]['obs_dim'] == systems[k]['input_dim']:
-#                 raise ValueError(f"Dimension mismatch between index {k}.obs_dim "
-#                                  f"({systems[k-1]['obs_dim']}) and index {k+1}.input_dim ({systems[k-1]['obs_dim']}) "
-#                                  f"(indexing from 1)")
-
-#     def prepare_data(self, y):
-#         """ y: torch tensor ([n_batchs,] n_times, obs_dim)"""
-#         if not y.shape[-1] == self._obs_dim:
-#             raise ValueError(
-#                 f"Dimension of measurements y ({y.shape[-1]} does not match declared obs_dim ({self._obs_dim}).")
-#         if len(y.shape) == 2:
-#             y = y.unsqueeze(0)
-#         elif len(y.shape) != 3:
-#             raise ValueError(
-#                 f"Expected measurements y to have 2 or 3 dimensions, but got ({len(y.shape)}.")
-#         return ydef predef prepare_data(self, y):
-#         """ y: torch tensor ([n_batchs,] n_times, obs_dim)"""
-#         if not y.shape[-1] == self._obs_dim:
-#             raise ValueError(
-#                 f"Dimension of measurements y ({y.shape[-1]} does not match declared obs_dim ({self._obs_dim}).")
-#         if len(y.shape) == 2:
-#             y = y.unsqueeze(0)
-#         elif len(y.shape) != 3:
-#             raise ValueError(
-#                 f"Expected measurements y to have 2 or 3 dimensions, but got ({len(y.shape)}.")
-#         return ypare_data(self, y):
-#         """ y: torch tensor ([n_batchs,] n_times, obs_dim)"""
-#         if not y.shape[-1] == self._obs_dim:
-#             raise ValueError(
-#                 f"Dimension of measurements y ({y.shape[-1]} does not match declared obs_dim ({self._obs_dim}).")
-#         if len(y.shape) == 2:
-#             y = y.unsqueeze(0)
-#         elif len(y.shape) != 3:
-#             raise ValueError(
-#                 f"Expected measurements y to have 2 or 3 dimensions, but got ({len(y.shape)}.")
-#         return y
-
-#     def to_generalized_coordinates(self, series): 
-#         """ series: torch tensor (n_batchs, n_times, dim) 
-#             inspired from spm_DEM_embed.m
-#             series_generalized = T * [series[t-order/2], dots, series[t+order/2]]
-#         """
-#         n_batchs, n_times, dim = series.shape
-#         p = self._order
-
-#         E = torch.zeros((p + 1, p + 1))
-#         times = torch.arange(n_times)
-
-#         # Create E_ij(t) (note that indices start at 0) 
-#         for i in range(p + 1): 
-#             for j in range(p + 1): 
-#                 E[i, j] = (i + 1 - int((p + 1) / 2))**(j) / np.math.factorial(j) 
-
-#         # Compute T
-#         T = torch.linalg.inv(E)
-
-#         # Compute the slices
-#         slices = []
-#         for t in times: 
-#             start = int(t - (p + 1) / 2)
-#             end = start + p + 1
-#             if start < 0: 
-#                 slices.append(slice(0, p + 1))
-#             elif end > n_times:
-#                 slices.append(slice(n_times - (p + 1), n_times))
-#             else: 
-#                 slices.append(slice(start, end))
-
-#         series_slices = torch.stack([series[:, _slice] for _slice in slices], dim=1)
-
-#         # series_slices is (n_batchs, n_times, order + 1, dim)
-#         # T is ( order+1, order+1)
-#         generalized_coordinates = torch.einsum('ijkl,mk->ijlm', series_slices, T)
-
-
-#         return generalized_coordinates
-
-#     def generalized_covariance(self, s):
-#         """ s is the roughtness of the noise process 
-#         """
-#         p = self._order
-#         k = torch.arange(p + 1)
-#         x = np.sqrt(2) * s
-#         r = np.cumprod(1 - 2 * k) / (x**(2*k))
-#         S = torch.zeros((p+1,p+1))
-#         for i in range(p + 1): 
-#             j = 2 * k - i
-#             filt = torch.logical_and(j >= 0, j < p + 1)
-#             S[i,j[filt]] = (-1) ** (i) * r[filt]
-#         R = torch.linalg.inv(S)
-#         return S, R
-
-#     def filter(self, y):
-#         y = self.prepare_data(y)
-#         y = self.generalized_coordinates(y)
-#         D = torch.diagonal(torch.ones(self._order), k=1)
-#         
